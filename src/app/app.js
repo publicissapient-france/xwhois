ee7946bcdb4e797c20b72dc06c14bb64a40c3dc4
Alexandre HEBERT <ahebert@xebia.fr>
2015-02-16 23:44:23 +0100

    externalize route logic in a custom provider to clean app.js

--- src/app/app.js
+++ src/app/app.js
@@ -7,42 +7,10 @@ angular.module('app.xwhois',
         'ngRoute', 'route-segment', 'view-segment'
     ])
 
-    .config(function ($routeProvider, $routeSegmentProvider, routes) {
+    .config(function ($routeProvider, $routeSegmentProvider) {
 
         $routeSegmentProvider.options.autoLoadTemplates = true;
         $routeProvider.otherwise({redirectTo: '/'});
-
-        // additionnal states found in routes configuration file
-        var rootDeep = -1;
-        var defaultRoutesDatas = {
-            untilResolved: {
-                templateUrl: 'components/loading/loading.html'
-            },
-            resolveFailed: {
-                templateUrl: 'error/error.html',
-                controller: 'HomeController'
-            }
-        };
-
-        function walkRoutes(_routes, parent, parentsUrl, parentsId) {
-            rootDeep++;
-            _.each(_routes, function (route, routeName) {
-                var routeId = !!parentsId ? parentsId + '.' + routeName : routeName;
-                var routeUrl = !!parentsUrl ? parentsUrl + route.url : route.url;
-                console.log(_.times(rootDeep, function () {
-                    return ' ';
-                }).join('') + routeName + '[' + routeId + '] : ' + routeUrl);
-                $routeSegmentProvider.when(routeUrl, routeId);
-                route = _.extend(route, defaultRoutesDatas);
-                parent.segment(routeName, route);
-                if (!!route['./']) {
-                    walkRoutes(route['./'], parent.within(routeName), routeUrl, routeName);
-                }
-            }, $routeSegmentProvider);
-            rootDeep--;
-        }
-
-        walkRoutes(routes, $routeSegmentProvider);
 
     })
 
---
+++ src/app/components/routes/routes-provider.js
@@ -0,0 +1,43 @@
+'use strict';
+
+angular.module('app.xwhois')
+
+    .provider('$routes', function ($routeSegmentProvider, routes) {
+
+        // additionnal states found in routes configuration file
+        var rootDeep = -1;
+        var defaultRoutesDatas = {
+            untilResolved: {
+                templateUrl: 'components/loading/loading.html'
+            },
+            resolveFailed: {
+                templateUrl: 'error/error.html',
+                controller: 'HomeController'
+            }
+        };
+
+        function walkRoutes(_routes, parent, parentsUrl, parentsId) {
+            rootDeep++;
+            _.each(_routes, function (route, routeName) {
+                var routeId = !!parentsId ? parentsId + '.' + routeName : routeName;
+                var routeUrl = !!parentsUrl ? parentsUrl + route.url : route.url;
+                console.log(_.times(rootDeep, function () {
+                    return ' ';
+                }).join('') + routeName + '[' + routeId + '] : ' + routeUrl);
+                $routeSegmentProvider.when(routeUrl, routeId);
+                route = _.extend(route, defaultRoutesDatas);
+                parent.segment(routeName, route);
+                if (!!route['./']) {
+                    walkRoutes(route['./'], parent.within(routeName), routeUrl, routeName);
+                }
+            }, $routeSegmentProvider);
+            rootDeep--;
+        }
+
+        walkRoutes(routes, $routeSegmentProvider);
+
+        this.$get = function() {
+            return routes;
+        };
+
+    });
