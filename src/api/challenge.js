db2f3bdea1c986d7cb59e1ace3bfefd1dff20596
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2015-03-03 07:08:40 +0100

    reformat code

--- src/api/challenge.js
+++ src/api/challenge.js
@@ -1,31 +1,30 @@
 var first = false;
 var challenge1 = {
-    firstImage : '/images/Sebastian Le Merdy.jpg',
-    secondImage : '/images/Antoine Michaud.jpg',
-    name : 'Sébastian Le Merdy',
-    answer : "firstImage"
+    firstImage: '/images/Sebastian Le Merdy.jpg',
+    secondImage: '/images/Antoine Michaud.jpg',
+    name: 'Sébastian Le Merdy',
+    answer: "firstImage"
 };
 var challenge2 = {
-    firstImage : '/images/Sebastian Le Merdy.jpg',
-    secondImage : '/images/Antoine Michaud.jpg',
-    name : 'Antoine Michaud',
-    answer : "secondImage"
+    firstImage: '/images/Sebastian Le Merdy.jpg',
+    secondImage: '/images/Antoine Michaud.jpg',
+    name: 'Antoine Michaud',
+    answer: "secondImage"
 };
 
-exports.createChallenge =  function (){
+exports.createChallenge = function () {
     return {
-        "reinit" : function () { // TODO this is for test puprose only : see how to remove it
+        "reinit": function () { // TODO this is for test puprose only : see how to remove it
             first = false;
             return this;
         },
-        "createChallenge" : function() {
+        "createChallenge": function () {
             first = !first;
-            if(first){
+            if (first) {
                 return challenge1;
-            }else {
+            } else {
                 return challenge2;
             }
         }
     };
 };
-
