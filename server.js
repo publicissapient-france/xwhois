fe8099a2c86fee9050b4c29b4e83f95f75b03f46
Alexandre HEBERT <ahebert@xebia.fr>
2015-03-07 11:03:53 +0100

    node serve front

--- server.js
+++ server.js
@@ -1,14 +1,30 @@
+
 var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var errorHandler = require('errorhandler');
+var methodOverride = require('method-override');
+
+var root = __dirname;
+var port = process.argv[2] || 8080;
 var challengeModule = require('./src/api/challenge');
-var app = express();
+var app = module.exports = express();
 
-app.get("/", function (req, res) {
-        res.writeHead(200);
-        res.end('Salut tout le monde !');
-});
+{ // configure server with error handlers, etc.
+    app.use(logger('dev'));
+    app.use(bodyParser.json());
+    app.use(cookieParser());
+    app.use(methodOverride('_method'));
+    if (app.get('env') === 'development') {
+        app.use(errorHandler());
+    }
+}
 
-app.get("/api/challenge", function (req, res) {
+app.get('/api/challenge', function (req, res) {
     res.send(challengeModule('./src/assets/images/xebians', '/assets/images/xebians').createChallenge());
 });
+app.use(express.static(path.join(root, './build/')));
 
-app.listen(8080);
+app.listen(port);
