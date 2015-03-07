9544b35043ac82c0608467a34dbef0afd989d584
Alexandre HEBERT <ahebert@xebia.fr>
2015-03-07 16:25:46 +0100

    added modal on leave

--- src/app/pages/match/match.controller.js
+++ src/app/pages/match/match.controller.js
@@ -6,14 +6,24 @@ angular.module('app.xwhois')
         var statusBar = angular.element(document.querySelector('.header'));
 
         $scope.question = null;
+        $scope.modal = false;
 
         $scope.giveUp = function () {
+            $scope.modal = false;
             $rootScope.stopping = true;
             $match.kill();
             statusBar.addClass('slide-out');
             $timeout(function () {
                 $location.path('/');
             }, 600);
+        };
+
+        $scope.openModal = function() {
+            $scope.modal = true;
+        };
+
+        $scope.closeModal = function() {
+            $scope.modal = false;
         };
 
         $match.start();
