d32da8f778918310b54626b091a4b91ca8a97eab
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2015-03-16 23:25:36 +0100

    parse attachments

--- src/api/trombinoscope.js
+++ src/api/trombinoscope.js
@@ -1,12 +1,12 @@
 var cheerio = require('cheerio'),
     confluence = require('./infrastructure/confluence');
 
-function extractImage(element) {
+function extractImage(element, index, self) {
     var image = element.find('ri\\:attachment').attr('ri:filename');
-    if (image === undefined) {
-        image = element.find('ri\\:url').attr('ri:value');
+    if (image !== undefined) {
+        self.getPeople(index)['filename'] = image;
     }
-    return image;
+    self.getPeople(index)['href'] = element.find('ri\\:url').attr('ri:value');
 }
 
 function sanitize(content) {
@@ -30,17 +30,60 @@ module.exports = {
         return this.people[index];
     },
 
+    'findPeople': function (filename) {
+        for (var i = 0; i < this.people.length; i++) {
+            if (this.people[i]['filename'] === filename) {
+                return this.people[i];
+            }
+        }
+        return undefined;
+    },
+
     'parse': function () {
         confluence.content(process.env.RESOURCE_ID, function (content) {
-            var $ = cheerio.load(cheerio.load(content).root().text()), self = this;
-
-            $('ac\\:image').each(function (index) {
-                self.getPeople(index)['image'] = extractImage($(this));
-            });
+            var $ = cheerio.load(cheerio.load(content).root().text()),
+                self = this;
 
             $('th').each(function (index) {
                 self.getPeople(index)['name'] = sanitize($(this).html());
             });
+
+            $('ac\\:image').each(function (index) {
+                extractImage($(this), index, self);
+            });
+
+            confluence.attachments(process.env.RESOURCE_ID, function (content) {
+                var $ = cheerio.load(content),
+                    urlByFilename = {};
+
+                $('attachment').each(function () {
+                    var attachment = $(this);
+                    var filename = attachment.attr('filename');
+                    attachment.find('link').each(function () {
+                        var link = $(this);
+                        if (link.attr('rel') === 'download') {
+                            urlByFilename[filename] = link.attr('href');
+                        }
+                    });
+                });
+
+                for (var filename in urlByFilename) {
+                    if (!urlByFilename.hasOwnProperty(filename)) {
+                        continue;
+                    }
+                    var people = self.findPeople(filename);
+                    if (people !== undefined) {
+                        delete people['filename'];
+                        people['href'] = urlByFilename[filename];
+                        continue;
+                    }
+                    console.log(filename, ' is not known');
+                }
+
+                console.log(self.people);
+            }, function (error) {
+                console.log(error);
+            })
         }, function (error) {
             console.log(error);
         });
