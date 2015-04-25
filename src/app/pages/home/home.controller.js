51a1d0e018db6374865869ef5c9292855f2b23c2
geekarist <christophe.pele@gmail.com>
2015-04-25 19:01:56 +0200

    Add signin button (WIP)

--- src/app/pages/home/home.controller.js
+++ src/app/pages/home/home.controller.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('app.xwhois')
-    .controller('HomeController', function ($navigation) {
+    .controller('HomeController', function ($navigation, $scope) {
         $navigation.leavingHome = false;
+
+        $scope.connected = false;
+
+        $scope.signin = function() {
+            $scope.connected = true;
+        };
     });
