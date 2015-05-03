e0a5d33bc84efb6246c847f7f00b9e7dd69a6371
geekarist <christophe.pele@gmail.com>
2015-05-03 10:42:32 +0200

    Fix logout
    When the user was logged out, the Play button was still displayed.
    
    This is fixed by using a function instead of a variable to get the
    status of the user.

--- src/app/pages/home/home.controller.js
+++ src/app/pages/home/home.controller.js
@@ -1,10 +1,10 @@
 'use strict';
 
 angular.module('app.xwhois')
-    .controller('HomeController', function ($navigation, $scope, $timeout, AccessToken) {
+    .controller('HomeController', function ($navigation, $scope, AccessToken) {
         $navigation.leavingHome = false;
 
-        $timeout(function() {
-            $scope.logged = !!AccessToken.get();
-        }, 0);
+        $scope.loggedIn = function () {
+            return !!AccessToken.get();
+        };
     });
