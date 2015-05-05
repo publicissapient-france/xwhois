2981303928602207b78599757e4956b6eec346b9
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2015-05-05 18:41:02 +0200

    Parametrize confluence user at launch

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-npm install && gulp build && CONFLUENCE_HOSTNAME=intranet.xebia.com CONFLUENCE_USER=cpele CONFLUENCE_PASSWORD="$(cat pass.txt)" CONFLUENCE_RESOURCE_ID=4522409 node server.js
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+    echo "Usage: $0 <CONFLUENCE_USER>"
+    exit 1
+fi
+
+npm install && gulp build && CONFLUENCE_HOSTNAME=intranet.xebia.com CONFLUENCE_USER=$1 CONFLUENCE_PASSWORD="$(cat pass.txt)" CONFLUENCE_RESOURCE_ID=4522409 node server.js
